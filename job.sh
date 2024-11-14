5884dfab1a707522f5d5dafc2fdaa51409e3033e
Jean Iaquinta <44261743+j34ni@users.noreply.github.com>
2024-11-14 12:57:09 +0100

    Update job.sh

--- job.sh
+++ job.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
 #SBATCH --job-name=namd2-container
-#SBATCH --account=ecxxxx
+#SBATCH --account=ecxx
 #SBATCH --time=04:00:00
-#SBATCH --mem-per-cpu=512
+#SBATCH --mem-per-cpu=2G
 #SBATCH --nodes=4
 #SBATCH --ntasks-per-node=32
 #SBATCH --cpus-per-task=1
@@ -17,4 +17,4 @@ set -o nounset # Treat unset variables as errors
 FILES=/location_of_the_data
 NAMD2="ubuntu2204_mpich421_namd2.sif"
 
-srun -n $SLURM_NTASKS --mpi=pmi2 apptainer exec --bind $FILES:/mnt $NAMD2 ...
+srun -n $SLURM_NTASKS --mpi=pmi2 apptainer exec --bind $FILES:/mnt $NAMD2 namd2 /mnt/1-3/complex/equ_site.namd
